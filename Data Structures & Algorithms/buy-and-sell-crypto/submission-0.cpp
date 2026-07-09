3dc88bfe2b5d37893432fe06b92cb477dcb067f6
Nandini Nema <146111602+nandininema07@users.noreply.github.com>
2026-07-09 17:24:27 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int profit=0,diff=0;
+        int ptr1=0, ptr2=1;
+        while(ptr2!=n){
+            if(prices[ptr1]>prices[ptr2]){
+                ptr1++;
+                ptr2++;
+            }
+            else{
+                diff=prices[ptr2]-prices[ptr1];
+                profit=max(profit,diff);
+                ptr2++;
+            }
+        }
+        return profit;
+    }
+};
